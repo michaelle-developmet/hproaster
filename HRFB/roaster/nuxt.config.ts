0c1aaf9dac8cdea6f171720098a58d68825bb38a
michaelle-developmet <99186895+michaelle-developmet@users.noreply.github.com>
2025-06-25 17:35:37 +0300

    docker

--- HRFB/roaster/nuxt.config.ts
+++ HRFB/roaster/nuxt.config.ts
@@ -1,13 +1,16 @@
+import shadcn from 'shadcn-nuxt'
+
+
 export default defineNuxtConfig({
   ssr: true,
 
   runtimeConfig: {
     public: {
-      serverUrl: process.env.SERVER_URL || 'http://localhost:5011'
+      serverUrl: process.env.SERVER_URL || 'http://13.49.46.74:5011'
     }
   },
 
-  modules: ['@nuxt/image', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  modules: ['@nuxt/image', '@nuxtjs/tailwindcss',     shadcn ],
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
